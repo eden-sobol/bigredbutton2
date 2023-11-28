6b0a142304fce18e3d7b0c523d3752cd02b0fc9e
eden-sobol <edenwesleysobol@gmail.com>
2023-11-28 14:30:57 -0500

    started save but not working

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,19 @@ export default function Index() {
 	let [blocked, setBlocked] = useState(false);
 	let [pressed, setPressed] = useState(false);
 
+	let [savedCount, setSaved] = useState(0);
+
 	let button = useRef();
 	let countElement = useRef();
 
 	useEffect(() => {
 		console.log("load");
 
+		setSaved(localStorage.setItem("count", "1"));
+
 		key();
+
+		countElement.innerHTML = localStorage.setItem("count", "1");
 	}, []);
 
 	function key() {
@@ -42,14 +48,16 @@ export default function Index() {
 					<p>Settings</p>
 				</div>
 				<div className={style.count}>
-					<p ref={countElement}>0</p>
+					<p ref={countElement}>
+						{savedCount}
+					</p>
 				</div>
 
 				<div className={style.save}>
 					<p>Save</p>
 				</div>
 				<div className={style.clear}>
-					<p>Clear Save</p>
+					<p>Clear</p>
 				</div>
 
 				<div ref={button} onClick={counter} className={`${style.button} ${pressed ? style.press: null
